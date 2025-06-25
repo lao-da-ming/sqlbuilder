e09aad9c0f1119af80aa720cdff001136fb504d9
laodaming <1059642220@qq.com>
2025-06-25 10:22:26 +0800

    最新

--- rule/rule.go
+++ rule/rule.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"github.com/duke-git/lancet/v2/slice"
 	"log"
-	"reflect"
-	"strings"
 )
 
 // 维度
@@ -140,6 +138,7 @@ func thirdLevel(ctx context.Context, loginEmployee int64, second map[DbField][]E
 	//其实只有一个元素
 	for field, value := range second {
 		var collections []any //集合
+		//TODO添加不通的字段在这里维护数据源
 		switch field {
 		case CreatedBy: //人员id
 			collections, err = createByDimensional(ctx, loginEmployee, value)
@@ -156,79 +155,4 @@ func thirdLevel(ctx context.Context, loginEmployee int64, second map[DbField][]E
 		break
 	}
 	return mapFieldValues, nil
-}
-
-// 构建最终的sql
-func buildSql(ctx context.Context, includeValues, excludeValues map[DbField][]any) (string, error) {
-	lengthInclude := len(includeValues)
-	lengthExclude := len(excludeValues)
-	//拼接sql
-	index := 0
-	sqlBuilder := strings.Builder{}
-	sqlBuilder.WriteString(" (")
-	for field, value := range includeValues {
-		index++
-		sqlBuilder.WriteString(string(field))
-		sqlBuilder.WriteString(" IN (")
-		sqlBuilder.WriteString(joinArrayElementForSql(value, ","))
-		sqlBuilder.WriteString(")")
-		if index != lengthInclude {
-			sqlBuilder.WriteString(" OR ")
-		}
-	}
-	sqlBuilder.WriteString(") ")
-	if lengthExclude == 0 {
-		return sqlBuilder.String(), nil
-	}
-	sqlBuilder.WriteString("AND (")
-	//初始化index
-	index = 0
-	for field, value := range excludeValues {
-		index++
-		sqlBuilder.WriteString(string(field))
-		sqlBuilder.WriteString(" NOT IN (")
-		sqlBuilder.WriteString(joinArrayElementForSql(value, ","))
-		sqlBuilder.WriteString(")")
-		if index != lengthExclude {
-			sqlBuilder.WriteString(" OR ")
-		}
-	}
-	sqlBuilder.WriteString(") ")
-	return sqlBuilder.String(), nil
-}
-
-// 处理别名
-func setAlias(ctx context.Context, includeValues, excludeValues map[DbField][]any, fieldAlias map[DbField]DbField) {
-	for field, alias := range fieldAlias {
-		if value, ok := includeValues[field]; ok {
-			includeValues[alias] = value
-			delete(includeValues, field)
-		}
-		if value, ok := excludeValues[field]; ok {
-			excludeValues[alias] = value
-			delete(excludeValues, field)
-		}
-	}
-}
-
-// 数组元素连接
-func joinArrayElementForSql(values []any, separator string) string {
-	lenValues := len(values)
-	if lenValues == 0 {
-		return ""
-	}
-	itemType := reflect.TypeOf(values[0]).String()
-	strBuilder := strings.Builder{}
-	switch itemType {
-	case "string": //字符串类型
-		for key, item := range values {
-			strBuilder.WriteString(fmt.Sprintf("'%v'", item))
-			if key != lenValues-1 {
-				strBuilder.WriteString(separator)
-			}
-		}
-	default: //其他类型型
-		strBuilder.WriteString(slice.Join(values, separator))
-	}
-	return strBuilder.String()
 }
---
+++ rule/utils.go
@@ -0,0 +1,84 @@
+package rule
+
+import (
+	"context"
+	"fmt"
+	"github.com/duke-git/lancet/v2/slice"
+	"reflect"
+	"strings"
+)
+
+// 构建最终的sql
+func buildSql(ctx context.Context, includeValues, excludeValues map[DbField][]any) (string, error) {
+	lengthInclude := len(includeValues)
+	lengthExclude := len(excludeValues)
+	//拼接sql
+	index := 0
+	sqlBuilder := strings.Builder{}
+	sqlBuilder.WriteString(" (")
+	for field, value := range includeValues {
+		index++
+		sqlBuilder.WriteString(string(field))
+		sqlBuilder.WriteString(" IN (")
+		sqlBuilder.WriteString(joinArrayElementForSql(value, ","))
+		sqlBuilder.WriteString(")")
+		if index != lengthInclude {
+			sqlBuilder.WriteString(" OR ")
+		}
+	}
+	sqlBuilder.WriteString(") ")
+	if lengthExclude == 0 {
+		return sqlBuilder.String(), nil
+	}
+	sqlBuilder.WriteString("AND (")
+	//初始化index
+	index = 0
+	for field, value := range excludeValues {
+		index++
+		sqlBuilder.WriteString(string(field))
+		sqlBuilder.WriteString(" NOT IN (")
+		sqlBuilder.WriteString(joinArrayElementForSql(value, ","))
+		sqlBuilder.WriteString(")")
+		if index != lengthExclude {
+			sqlBuilder.WriteString(" OR ")
+		}
+	}
+	sqlBuilder.WriteString(") ")
+	return sqlBuilder.String(), nil
+}
+
+// 处理别名
+func setAlias(ctx context.Context, includeValues, excludeValues map[DbField][]any, fieldAlias map[DbField]DbField) {
+	for field, alias := range fieldAlias {
+		if value, ok := includeValues[field]; ok {
+			includeValues[alias] = value
+			delete(includeValues, field)
+		}
+		if value, ok := excludeValues[field]; ok {
+			excludeValues[alias] = value
+			delete(excludeValues, field)
+		}
+	}
+}
+
+// 数组元素连接
+func joinArrayElementForSql(values []any, separator string) string {
+	lenValues := len(values)
+	if lenValues == 0 {
+		return ""
+	}
+	itemType := reflect.TypeOf(values[0]).String()
+	strBuilder := strings.Builder{}
+	switch itemType {
+	case "string": //字符串类型
+		for key, item := range values {
+			strBuilder.WriteString(fmt.Sprintf("'%v'", item))
+			if key != lenValues-1 {
+				strBuilder.WriteString(separator)
+			}
+		}
+	default: //其他类型型
+		strBuilder.WriteString(slice.Join(values, separator))
+	}
+	return strBuilder.String()
+}
